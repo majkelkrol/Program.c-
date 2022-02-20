49b60d98147d6d13323268c26d769cdb8066f96e
Majkel <majkelkrol@icloud.com>
2022-02-20 20:04:36 +0100

    initial commit

---
+++ GIT/Program.c++/Program.c++/main.cpp
@@ -0,0 +1,31 @@
+//
+//  main.cpp
+//  Program.c++
+//
+//  Created by Majkel on 20/02/2022.
+//
+
+#include <iostream>
+using namespace std;
+
+struct Car {
+    string name;
+    int cost;
+    int power;
+    string drive;
+};
+
+int main() {
+    
+    Car car;
+    car.name = "BWW";
+    car.cost = 350.000;
+    car.power = 370;
+    car.drive = "xDrive";
+    
+    cout << car.name << endl;
+    cout << CAR.drive << endl;
+    cout << carcost << endl;
+    
+    return 0;
+}
